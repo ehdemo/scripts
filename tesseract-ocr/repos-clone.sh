43b2b636cf8b5ab697c136c18000a4ee1c68a76c
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-01-19 01:47:00 +0530

    Add tesseract repos clone

---
+++ tesseract-ocr/repos-clone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Clone tesseract
+git clone https://github.com/tesseract-ocr/tesseract
+
+# Clone leptonica
+git clone https://github.com/DanBloomberg/leptonica
+
+# Clone langdata
+git clone https://github.com/tesseract-ocr/langdata tesseract/langdata
+
+# Download languages
+wget https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata -P tesseract/tessdata
+wget https://github.com/tesseract-ocr/tessdata/raw/master/sin.traineddata -P tesseract/tessdata
+
+export TESS_SRC_DIR=${PWD}/tesseract
+export LEPT_SRC_DIR=${PWD}/leptonica

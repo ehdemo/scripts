a95451a223258c324c5897e3862c3ff0f4ebb0da
Sumedhe Dissanayake <sumedhe5dms@gmail.com>
2018-03-07 23:59:29 +0530

    Modify compile leptonica

--- tesseract-ocr/compile-leptonica.sh
+++ tesseract-ocr/compile-leptonica.sh
@@ -2,6 +2,19 @@
 
 cd ${LEPT_SRC_DIR}
 
+# Install dependencies
+sudo apt-get install -y g++  \
+   autoconf automake libtool \
+   autoconf-archive \
+   pkg-config \
+   libpng-dev \
+   libjpeg8-dev \
+   libtiff5-dev \
+   zlib1g-dev \
+   libicu-dev \
+   libpango1.0-dev \
+   libcairo2-dev
+
 # Configure
 ./autobuild
 ./configure
